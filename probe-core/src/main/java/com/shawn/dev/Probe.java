6d563e08180fefa4522dac25920f1990262ce037
shawn417 <qijun.shen@outlook.com>
2016-01-26 21:04:21 +0800

    changed Probe to singleton mode

--- probe-core/src/main/java/com/shawn/dev/Probe.java
+++ probe-core/src/main/java/com/shawn/dev/Probe.java
@@ -4,4 +4,22 @@ package com.shawn.dev;
  * Created by Qijun on 2016/1/25.
  */
 public class Probe {
+
+    private static Probe probe = null;
+
+    private Probe() {
+
+    }
+
+    public static Probe build() {
+        if (probe == null) {
+            synchronized (Probe.class) {
+                if (probe == null) {
+                    probe = new Probe();
+                }
+            }
+        }
+        return probe;
+    }
+
 }
